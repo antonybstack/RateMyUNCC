adb1d54507f9aff0c59ff1dae88b3c2daf4c712e
antonybstack <ablyakhe@uncc.edu>
2020-06-17 17:54:35 -0400

    exteme cases handling

--- script.js
+++ script.js
@@ -10,18 +10,37 @@ function numOfProfessors() {
 numOfProfessors();
 
 function searchName(nameArray, professorArray) {
-  let hyphenLast = nameArray[nameArray.length - 2].concat("- ", nameArray[nameArray.length - 1]);
-  let hyphenFirst = nameArray[nameArray.length - 2].concat("- ", nameArray[nameArray.length - 1]);
+  let hyphenLast1;
+  let hyphenLast2;
+  console.log(nameArray);
+  nameArray[0] = nameArray[0].toLowerCase();
+  nameArray[1] = nameArray[1].toLowerCase();
+  if (nameArray[2]) {
+    nameArray[2] = nameArray[2].toLowerCase();
+  }
+  console.log(nameArray);
+  if (nameArray !== undefined && nameArray.length > 2) {
+    hyphenLast1 = nameArray[nameArray.length - 2].concat("- ", nameArray[nameArray.length - 1]);
+    hyphenLast2 = nameArray[nameArray.length - 2].concat("-", nameArray[nameArray.length - 1]);
+    combinedMiddleLast = nameArray[nameArray.length - 2].concat(" ", nameArray[nameArray.length - 1]);
+  }
+
   for (var i = 0; i < professorArray.length; i++) {
-    if (
-      professorArray[i].tFname.toLowerCase() === nameArray[0].toLowerCase() &&
-      professorArray[i].tLname.toLowerCase() === nameArray[nameArray.length - 1].toLowerCase() &&
-      professorArray[i].tNumRatings > 0
-    ) {
+    if (professorArray[i].tFname === undefined || professorArray[i].tLname === undefined) {
+      console.log("oops!");
+    }
+    if (professorArray[i].tFname.toLowerCase() === nameArray[0] && professorArray[i].tLname.toLowerCase() === nameArray[nameArray.length - 1] && professorArray[i].tNumRatings > 0) {
       return professorArray[i];
-    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0].toLowerCase() && professorArray[i].tLname.toLowerCase() === hyphenLast.toLowerCase()) {
+    }
+    if (professorArray[i].tFname.toLowerCase() === nameArray[1] && professorArray[i].tLname.toLowerCase() === nameArray[nameArray.length - 1] && professorArray[i].tNumRatings > 0) {
       return professorArray[i];
-    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0].concat(" ").toLowerCase() && professorArray[i].tLname.toLowerCase() === nameArray[nameArray.length - 1].toLowerCase()) {
+    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0] && professorArray[i].tLname.toLowerCase() === hyphenLast1) {
+      return professorArray[i];
+    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0] && professorArray[i].tLname.toLowerCase() === hyphenLast2) {
+      return professorArray[i];
+    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0] && professorArray[i].tLname.toLowerCase() === combinedMiddleLast) {
+      return professorArray[i];
+    } else if (professorArray[i].tFname.toLowerCase() === nameArray[0].concat(" ") && professorArray[i].tLname.toLowerCase() === nameArray[nameArray.length - 1]) {
       return professorArray[i];
     }
   }
@@ -51,21 +70,70 @@ function run() {
   var tableRef = $(".datadisplaytable");
   tableRef.find("tbody tr th:nth-child(20)").after("<th class=ddheader scope=col id=rateCol>Rating</th>");
   tableRef.find("tbody tr").each(function (i) {
+    var placement = 20;
+    var level = "dne";
+    if ($(this).find("td:nth-child(9)").text() === "TBA") {
+      placement = 19;
+    }
     if (i > 1) {
-      if ($(this).find("td:nth-child(20)").text() !== "TBA") {
-        let name = $.trim($(this).find("td:nth-child(20)").text());
+      if (
+        $(this)
+          .find("td:nth-child(" + placement + ")")
+          .text() !== "TBA"
+      ) {
+        let name = $.trim(
+          $(this)
+            .find("td:nth-child(" + placement + ")")
+            .text()
+        );
         let professor = name.replace("  ", " ");
+        professor = professor.replace("'", "");
         professor = professor.split(" ");
         if (professor[professor.length - 1] === "(P)") {
           professor.pop();
         }
+        professor = professor.filter(function (element) {
+          return element !== "";
+        });
+
+        if (professor.length > 4) {
+          professor.splice(1, professor.length - 2);
+        }
+
+        //last name has hyphen
         if (professor[professor.length - 1].includes("-")) {
+          console.log(professor);
           let temp = professor.pop();
           let tempArr = [];
           tempArr = temp.split("-");
           tempArr.forEach((element) => professor.push(element));
-          professor.splice(1, 1);
+          if (professor.length === 4) {
+            professor.splice(1, 1);
+          }
         }
+        //first name has hyphen
+        if (professor[0].includes("-")) {
+          if (professor.length === 2) {
+            let tempLast = professor.pop();
+            let tempFirst = professor.pop();
+            let tempArr = [];
+            tempArr = tempFirst.split("-");
+            tempArr.forEach((element) => professor.push(element));
+            professor.push(tempLast);
+            professor.splice(1, 1);
+          } else if (professor.length === 3) {
+            let tempLast = professor.pop();
+            let tempMiddle = professor.pop();
+            let tempFirst = professor.pop();
+            let tempArr = [];
+            tempArr = tempFirst.split("-");
+            tempArr.forEach((element) => professor.push(element));
+            professor.push(tempMiddle);
+            professor.push(tempLast);
+            professor.splice(2, 1);
+          }
+        }
+
         let rating;
         let id;
         let professorProfile = searchName(professor, profList);
@@ -73,7 +141,6 @@ function run() {
           rating = professorProfile.overall_rating;
           id = professorProfile.tid;
         }
-        let level;
         switch (true) {
           case rating >= 4:
             level = "great";
@@ -96,17 +163,19 @@ function run() {
         }
         if (rating !== undefined) {
           $(this)
-            .find("td:nth-child(20)")
+            .find("td:nth-child(" + placement + ")")
             .after(
               "<td class=dddefault id=" + level + "><a class=rmpLink style='color:white !important;' href=https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + id + ">" + rating + "</a></td>"
             );
         } else {
           $(this)
-            .find("td:nth-child(20)")
+            .find("td:nth-child(" + placement + ")")
             .after("<td class=dddefault id=" + level + ">DNE</td>");
         }
       } else {
-        $(this).find("td:nth-child(20)").after("<td class=dddefault id=rateRow>N/A</td>");
+        $(this)
+          .find("td:nth-child(" + placement + ")")
+          .after("<td class=dddefault id=" + level + ">N/A</td>");
       }
     }
     // crosses out the classes that are closed
@@ -118,9 +187,7 @@ function run() {
 
 //checks if professor list is loaded
 function checkVariable() {
-  console.log(profList);
   if (profList.length > 2979) {
-    console.log(profList);
     return true;
   } else {
     return false;
@@ -135,7 +202,6 @@ var interval1 = setInterval(function () {
 }, 50);
 
 var interval2 = setInterval(function () {
-  console.log("hello");
   if (checkVariable() === true) {
     clearInterval(interval1);
     clearInterval(interval2);
